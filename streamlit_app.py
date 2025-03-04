3981a25e33a90b5f19abead95d3525964d36ae21
smbindyo <solomonmbindyo@gmail.com>
2025-03-04 18:42:55 +0000

    basic code

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,13 @@ import streamlit as st
 st.title("Model1 deployment web app")
 #header
 st.header('Loan prediction web app')
-
+#paragraph
 st.write('This is a web app for loan predictions. This app takes user information and predicts his/her eligibility')
-
+st.markdown('This is a test project')
+#subheader
+st.subheader('Data info')
+st.write('some info on the data')
+#linear equation
+st.latex(r"y = \beta_0x_0+\beta_1x_1+\cdots\beta_nx_n+\epsilon")
+#caption
+st.caption('Linear regression1')
